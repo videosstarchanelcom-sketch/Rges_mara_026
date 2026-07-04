b057d734216e689ef9911cf76db5d2a511ec0496
videosstarchanelcom-sketch <videosstarchanel.com@gmail.com>
2026-07-04 07:46:04 -0400

    Update app.js
    actualizando accesos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // =================================================================
 // =================================================================
-const SUPABASE_URL = 'https://tu-proyecto.supabase.co'; // 
-const SUPABASE_ANON_KEY = 'tu-anon-key-aqui'; // 
+const SUPABASE_URL = 'https://supabase.com/dashboard/project/mveqwpsmgtvlmzdcbxkj'; // 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12ZXF3cHNtZ3R2bG16ZGNieGtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwODA4NzAsImV4cCI6MjA5ODY1Njg3MH0.JKecCwzBgS7cePVJVEhWnmLIGgcwunz-lU16EeRmeUY'; // 
 
 // =================================================================
 // =================================================================
